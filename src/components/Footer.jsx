003519e2d7796f4e4b660051323780c312e0e112
eyelid2003 <d.eyelid2003@gmail.com>
2026-02-25 14:05:35 +0530

    25-02 02

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,13 +10,13 @@ export default function Footer() {
 
       {/* Main Footer */}
       <div className="bg-white text-[#3a1667] py-6 text-center text-sm border-t border-[#ead6ff]">
-        <div className="max-w-6xl mx-auto px-6 grid md:grid-cols-3 gap-12">
+        <div className="max-w-6xl mx-auto px-6 grid md:grid-cols-4 gap-12">
 
           {/* About Section */}
           <div>
             <h2 className="text-2xl font-bold text-[#471d7d] mb-4">YaraPay - Recharge App</h2>
             <p className="text-[#3a1667] font-medium leading-relaxed">
-              Our user-friendly interface allows you to easily recharge your mobile phones, 
+              Our user-friendly interface allows you to easily recharge your mobile phones,
               DTH services, and book travel — all from one secure app.
             </p>
           </div>
@@ -65,23 +65,32 @@ export default function Footer() {
           {/* Contact Details */}
           <div>
             <h2 className="text-2xl font-bold text-[#471d7d] mb-4">Contact Details</h2>
-            <div className="space-y-2 text-[#3a1667] font-medium">
+            <div className="space-y-2 text-[#3a1667] font-medium text-center">
 
               <div className="flex items-center justify-center space-x-2">
                 <MapPin className="w-5 h-5 text-[#7c3aed]" />
-                <span className="w-[160px]">Uttar Pradesh, 261501</span>
+                <span className="w-auto">Uttar Pradesh, 261501</span>
               </div>
 
               <div className="flex items-center justify-center space-x-2">
                 <Mail className="w-5 h-5 text-[#7c3aed]" />
-                <span className="w-[150px]">yarapay@zohomail.in</span>
+                <span className="w-auto">yarapay@zohomail.in</span>
               </div>
 
               <div className="flex items-center justify-center space-x-2">
                 <Phone className="w-5 h-5 text-[#7c3aed]" />
-                <span className="w-[150px]">+91 8887990055</span>
+                <span className="w-auto">+91 8887990055</span>
               </div>
 
+            </div>
+          </div>
+
+          {/* GST Details */}
+          <div>
+            <h2 className="text-2xl font-bold text-[#471d7d] mb-4">GST Details</h2>
+            <div className="space-y-2 text-[#3a1667] font-medium">
+              <p>Name: YAGYADUTT SHARMA</p>
+              <p>GST Number: 09IILPS7906E1ZM</p>
             </div>
           </div>
 
